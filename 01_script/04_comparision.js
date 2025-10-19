823e0e7de3f6bb857bccd6b15b343b7ced2f78a8
Monugya | curious programmer <132900296+monugya-07@users.noreply.github.com>
2025-10-19 09:43:48 +0000

    Commit

---
+++ 01_script/04_comparision.js
@@ -0,0 +1,24 @@
+// console.log(2 > 1)
+// console.log(2 >=1 )
+// console.log(2 < 1)
+// console.log(2 == 1)
+// console.log(2 != 1)
+
+
+// Confusing comparisions ( We do avoid most of the times )
+
+console.log("2" > 1)
+console.log("02" > 1)
+
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0)
+
+console.log(undefined == 0)
+console.log(undefined > 0)
+console.log(undefined < 0)
+
+
+// === 
+
+console.log("2" === 2)
