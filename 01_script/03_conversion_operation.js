b0be3f038c6f87d713577c793f3917d2fbb4b137
Monugya | curious programmer <132900296+monugya-07@users.noreply.github.com>
2025-10-19 06:31:19 +0000

    Commit

---
+++ 01_script/03_conversion_operation.js
@@ -0,0 +1,42 @@
+// let score = 33
+
+// let score = "33"
+
+// let score = "33absdef"
+
+// let score = null
+
+// let score = undefined
+
+// let score = true
+
+let score = "Monugya"
+
+
+console.log(typeof score)
+console.log(typeof (score))
+
+
+let valueInNmber = Number(score)
+
+console.log(typeof valueInNmber)
+console.log(valueInNmber)
+
+
+// let isLoggedIn = 1
+
+// let isLoggedIn = ""
+
+let isLoggedIn = "Monugya"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn)
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber)
+console.log(typeof stringNumber)
