b61f20ddb92baafc90928f00540f6cf60a4f5382
Monugya | curious_coder <132900296+BinaryWhizz@users.noreply.github.com>
2025-11-12 07:57:26 +0000

    Add some more definition

--- 03_script/01_functions.js
+++ 03_script/01_functions.js
@@ -13,9 +13,9 @@ myName()                  // function call
 
 
 
-// function addTwoNumbers(number1, number2){            // number1, number2 are called parameters 
-//     console.log(number1 + number2 )
-// }
+function addTwoNumbers(number1, number2){            // number1, number2 are called parameters 
+    console.log(number1 + number2 )
+}
 
 
 function addTwoNumbers(number1, number2){            
@@ -73,3 +73,56 @@ function logInUserMessage(username = "Monugya"){
 console.log(logInUserMessage("Monugya"))  // Asked to print : Monugya
 console.log(logInUserMessage())           // Check it won't pass anything  
 console.log(logInUserMessage("MBC"))      // Overwrite by MBC
+
+
+function calculatecartPrice(num1) {
+    return num1
+}
+
+// console.log(calculatecartPrice(200))
+
+
+function calculatecartPrice(...num1) {                  // three ... 
+    return num1
+}
+
+console.log(calculatecartPrice(200, 400, 500))
+
+
+function calculateCartPrice(val1, val2, ...num1) {                 
+    return num1
+}
+
+console.log(calculateCartPrice(200, 400, 500, 2000))    // [ 500, 2000 ], 200 and 400 went inside val1 and val2
+
+
+const user = {
+    username1: "Monugya",
+    price: 199,
+}
+
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.username1} and price is ${anyobject.price}`) 
+    
+}
+
+handleObject(user)
+
+// also , passing direct object 
+
+// handleObject({
+//     username : "Sam",
+//     price : 399
+// })
+
+// also , passing array 
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray))
+console.log(returnSecondValue([200, 400, 500, 1000]))
+
