7f8605943235d6a481c994f7d6ec215ad601a1f2
Monugya | curious_coder <monugyaborchetia1@gmail.com>
2025-11-13 18:21:55 +0000

    JS skills - scopes

--- 03_script/02_scopes.js
+++ 03_script/02_scopes.js
@@ -1,27 +1,26 @@
-// // Scopes
+// Scopes
 
-// if (true) {
-//     let a = 10
-//     const b = 20
-//     var c = 30    
-// }
+if (true) {
+    let a = 10
+    const b = 20
+    var c = 30    
+}
 
-// // console.log(a)   // Won't be printed 
-// // console.log(b)   // Won't be printed
-// console.log(c)      // 30, people avoids var because of creating problems 
+// console.log(a)   // Won't be printed 
+// console.log(b)   // Won't be printed
+console.log(c)      // 30, people avoids var because of creating problems 
 
 
 
-// var c = 300              // Global scope
+var c = 300              // Global scope
 
-// if (true) {              
-//     let a = 10
-//     const b = 20
-//     var c = 30           // { var c = 30 }, block scope 
-// }
+if (true) {              
+    let a = 10
+    const b = 20
+    var c = 30           // { var c = 30 }, block scope 
+}
 
-// console.log(c)      // 30 
-
+console.log(c)      // 30 
 
 
 
@@ -38,5 +37,73 @@ if (true) {
 console.log(a)
 
 
-
 // Note : global scope in inspect --- console --- and global scope in code terminal --- both are different 
+
+
+
+// Nested scope
+
+function one(){
+    const username = "Monugya"
+
+    function two(){
+        const website = "Youtube"
+        console.log(username)
+    }
+
+    // console.log(website)          //Error
+
+    two()
+    
+}
+
+one()
+
+
+if (true) {
+    const username = "Monugya"
+    if (username === "Monugya") {
+        const website = "Youtube"
+        console.log(username + website)
+    }
+    console.log(website)        // Error
+    
+}
+
+console.log(username)           // Error
+
+
+
+// -------------------------------------- Interesting ------------------------------------ 
+
+function addOne(num){
+    return num + 1
+}
+
+addOne(5)
+
+
+// const addTwo = function(num){
+//     return num + 2 
+// }
+
+// addTwo(5)
+
+
+// Now try this 
+
+
+console.log(addOne(5))         // 6 , Will be discussed later 
+
+function addOne(num){
+    return num + 1
+}
+
+
+
+addTwo(5)                      // Error , Will be discussed later 
+
+const addTwo = function(num){
+    return num + 2 
+}
+
